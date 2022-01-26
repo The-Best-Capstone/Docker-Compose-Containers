cdf2f0f9ca239d9c015a185b1a50c4b028b3ddd9
HaraseesG <harasees99@gmail.com>
2022-01-25 20:10:47 -0500

    Feat: Multithreaded producer is now producing dummy data across 20 dummy channels Json mapping does not make sense yet

--- producer/producer.py
+++ producer/producer.py
@@ -1,13 +1,33 @@
-from time import sleep
-from json import dumps
+from json import load, dumps
 from kafka import KafkaProducer
+from time import sleep
+from random import uniform
+from threading import Thread
 
-producer = KafkaProducer(
-    bootstrap_servers=['172.17.0.1:9092'],
-    value_serializer=lambda x: dumps(x).encode('utf-8')
-)
-for j in range(9999):
-    print("Iteration", j)
-    data = j
-    producer.send('topic_test', value=data)
-    sleep(0.5)
+
+class SensorThread(Thread):
+    def __init__(self, current_sensor):
+        Thread.__init__(self)
+        self.current_sensor = current_sensor
+
+    def run(self):
+        try:
+            producer = KafkaProducer(
+                bootstrap_servers=['172.17.0.1:9092'],
+                value_serializer=lambda x: dumps(x).encode('utf-8')
+            )
+        except:
+            print("Error!!")
+
+        while True:
+            randInt = round(uniform(15.000, 300.000), 3)
+            print(self.current_sensor, randInt)
+            producer.send('topic_test', value=randInt)
+            sleep(1)
+
+if __name__ == "__main__":
+    with open("./config.json", "r") as f:
+        config = load(f)
+
+    for channel in config:
+        SensorThread(channel).start()
