9a331df757f2e15f78a3fef95469b2c83090d8fb
Eric <ericwej@gmail.com>
2022-01-30 14:43:03 -0500

    added stop script

--- stop.sh
+++ stop.sh
@@ -1,16 +1,17 @@
-cd kafka
-docker-compose down
-
-cd ../timescaledb 
-docker-compose down
-
-cd ../grafana 
+cd producer 
 docker-compose down
 
 cd ../consumer
 docker-compose down
 
-cd ../producer 
+cd ../kafka
 docker-compose down
 
+cd ../grafana 
+docker-compose down
+
+cd ../timescaledb 
+docker-compose down
+
+# print 
 docker ps -a
