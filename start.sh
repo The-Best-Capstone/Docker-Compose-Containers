290d28fceb26d485019156bc0277c1f53ac970c2
HaraseesG <harasees99@gmail.com>
2022-01-30 21:34:04 -0500

    Fix: start script no longer stops executing commands af after building the consumer

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 cd kafka
-docker-compose up -d
+docker-compose up -d &&\
 
 cd ../timescaledb 
-docker-compose up -d
+docker-compose up -d &&\
 
 cd ../grafana 
-docker-compose up -d 
+docker-compose up -d &&\
 
 cd ../consumer
-docker-compose up -d 
+docker-compose up -d &&\ 
 
 cd ../producer 
 docker-compose up -d
